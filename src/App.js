55641e069a4f384b8cc38d6951b9b35496cf97e3
jt-lamorak <jacob@lamorak.net>
2023-10-18 07:58:53 +0100

    minor code cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Converter from './Converter';
 import Header from './Header';
 import Footer from './Footer';
