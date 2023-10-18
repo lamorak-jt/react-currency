e5d7ae856320295fce1978b30b11eb1688d82e70
jt-lamorak <jacob@lamorak.net>
2023-10-18 07:49:56 +0100

    chart added

--- src/Converter.js
+++ src/Converter.js
@@ -3,6 +3,7 @@ import { json, checkStatus } from './utils';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css';
 import './Converter.css';
+import Chart from 'chart.js/auto';
 
 class Converter extends React.Component {  
   constructor(props) {
@@ -15,12 +16,62 @@ class Converter extends React.Component {
       targetAmount: '',
       dropdownOptions: [],
       error: '',
-    }
+    };
     this.onSelectBase = this.onSelectBase.bind(this);
     this.onSelectTarget = this.onSelectTarget.bind(this);
     this.fetchData = this.fetchData.bind(this);
     this.onChangeBaseAmount = this.onChangeBaseAmount.bind(this);
-    this.onChangeTargetAmount = this.onChangeTargetAmount.bind(this);   
+    this.onChangeTargetAmount = this.onChangeTargetAmount.bind(this);
+    this.getHistoricalRates = this.getHistoricalRates.bind(this);
+    //check that was right?
+    this.chartRef = React.createRef(); 
+  }
+
+  getHistoricalRates = (baseCurrency, targetCurrency) => {
+    const endDate = new Date().toISOString().split('T')[0];
+    const startDate = new Date((new Date).getTime() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+    fetch(`https://api.frankfurter.app/${startDate}..${endDate}?from=${baseCurrency}&to=${targetCurrency}`)
+      .then(checkStatus)
+      .then(json)
+      .then(data => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        const chartLabels = Object.keys(data.rates);
+        const chartData = Object.values(data.rates).map(rate => rate[targetCurrency]);
+        const chartLabel = `${baseCurrency}/${targetCurrency}`;
+        this.buildChart(chartLabels, chartData, chartLabel);
+      })
+      .catch(error => console.error(error.message));
+  }
+  buildChart = (labels, data, label) => {
+    const chartRef = this.chartRef.current.getContext("2d");
+    if (typeof this.chart !== "undefined") {
+      this.chart.destroy();
+    }
+    this.chart = new Chart(this.chartRef.current.getContext("2d"), {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [
+          {
+            label: label,
+            data,
+            fill: false,
+            tension: 0,
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+              display: false
+          },
+        },
+      }
+    })
   }
 
   fetchData(baseCurrency, targetCurrency, element) {
@@ -51,7 +102,8 @@ class Converter extends React.Component {
     console.log(event.value);
     let baseCurrency = event.value;
     let targetCurrency = this.state.targetCurrency;
-    this.fetchData(baseCurrency, targetCurrency, 'base');    
+    this.fetchData(baseCurrency, targetCurrency, 'base');
+    this.getHistoricalRates(baseCurrency, targetCurrency);
   }
 
   onSelectTarget(event) {    
@@ -59,7 +111,8 @@ class Converter extends React.Component {
     console.log(event.value);
     let baseCurrency = this.state.baseCurrency;
     let targetCurrency = event.value;
-    this.fetchData(baseCurrency, targetCurrency, 'target');    
+    this.fetchData(baseCurrency, targetCurrency, 'target');
+    this.getHistoricalRates(baseCurrency, targetCurrency);
   }
 
   onChangeBaseAmount(event) {   
@@ -95,6 +148,7 @@ class Converter extends React.Component {
         }) 
     console.log(this.state);
     this.fetchData(this.state.baseCurrency, this.state.targetCurrency);
+    this.getHistoricalRates(this.state.baseCurrency, this.state.targetCurrency);
     
   }
 
@@ -104,19 +158,24 @@ class Converter extends React.Component {
     let { baseCurrency, targetCurrency, rate, baseAmount, targetAmount, dropdownOptions } = this.state;
     
     return (      
-    <div class='container converter-container'>
-      <div class='converter-base'>
-        <input class='converter-amount' name='base-amount' type='number' value={baseAmount} onChange={this.onChangeBaseAmount} />
-        <div class='converter-dropdown' type='dropdown' name='base-currency'>
-          <Dropdown options={dropdownOptions} onChange={this.onSelectBase} value={baseCurrency} />
+    <div class='container converter-component-container'>
+      <div class='container converter-container'>
+        <div class='converter-base'>
+          <input class='converter-amount' name='base-amount' type='number' value={baseAmount} onChange={this.onChangeBaseAmount} />
+          <div class='converter-dropdown' type='dropdown' name='base-currency'>
+            <Dropdown options={dropdownOptions} onChange={this.onSelectBase} value={baseCurrency} />
+          </div>
         </div>
+        <div class='converter-equals'>=</div>
+        <div class='converter-target'>
+          <input class='converter-amount' name='target-amount' type='number' value={targetAmount} onChange={this.onChangeTargetAmount} />
+          <div class='converter-dropdown' type='dropdown' name='target-currency'>
+            <Dropdown options={dropdownOptions} onChange={this.onSelectTarget} value={targetCurrency} />
+          </div>
+        </div>            
       </div>
-      <div class='converter-equals'> = </div>
-      <div class='converter-target'>
-        <input class='converter-amount' name='target-amount' type='number' value={targetAmount} onChange={this.onChangeTargetAmount} />
-        <div class='converter-dropdown' type='dropdown' name='target-currency'>
-          <Dropdown options={dropdownOptions} onChange={this.onSelectTarget} value={targetCurrency} />
-        </div>
+      <div class='container container-canvas-chart'>
+        <canvas id='canvas-line-chart' ref={this.chartRef} />
       </div>
     </div>
     )
